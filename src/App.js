3c26620bb6e09e1403659d27c9b4302100c42736
Maria Castella <m.castella@matadorprime.com>
2023-04-12 19:26:31 +0200

    build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,149 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import quotes from './quotes.jsx';
+import images from './images.jsx';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      index: null,
+      indexImg: null,
+      allIndexes: [],
+      allImgIndexes: [],
+    }
+  }
+
+  componentDidMount = () => {
+    let firstIndex = this.randomIntFromInterval(1, quotes.length);
+    let firstIndexImg = this.randomIntFromInterval(1, images.length);
+
+    this.setState({
+      index: firstIndex,
+      indexImg: firstIndexImg,
+      allIndexes: [...this.state.allIndexes, firstIndex],
+      allImgIndexes: [...this.state.allIndexes, firstIndexImg],
+
+    })
+  }
+
+  randomIntFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+
+  handleClick = () => {
+    console.log("index", this.state.index)
+    console.log("indexImg", this.state.indexImg)
+
+    console.log("allIndexes", this.state.allIndexes)
+    console.log("allIMGIndexes", this.state.allImgIndexes)
+
+    const newIndex = this.randomIntFromInterval(1, quotes.length)
+    const newImgIndex = this.randomIntFromInterval(1, images.length)
+    let allIndexes = [...this.state.allIndexes]
+    let allImgIndexes = [...this.state.allImgIndexes]
+
+    allIndexes.push(newIndex)
+    allImgIndexes.push(newImgIndex)
+
+    this.setState({
+      index: newIndex,
+      indexImg: newImgIndex,
+      allIndexes: allIndexes,
+      allImgIndexes: allImgIndexes,
+    }, () => {
+      document.querySelector(".bg").classList.add("animate")
+      document.querySelector(".blockquote").classList.add("animate")
+    })
+  }
+
+  handleClickPrev = () => {
+
+    let myNewIndex = this.state.allIndexes.findIndex(el => el === this.state.index) - 1;
+    let myNewIndexStory = this.state.allIndexes[myNewIndex < 0 ? 0 : myNewIndex];
+
+    let myNewImgIndex = this.state.allImgIndexes.findIndex(el => el === this.state.indexImg) - 1;
+    let myNewImgIndexStory = this.state.allImgIndexes[myNewImgIndex < 0 ? 0 : myNewImgIndex];
+
+    this.setState({
+      index: myNewIndexStory,
+      indexImg: myNewImgIndexStory
+    }, () => {
+      document.querySelector(".bg").classList.add("animate")
+      document.querySelector(".blockquote").classList.add("animate")
+    })
+  }
+
+  handleClickNext = () => {
+
+    let myNewIndex = this.state.allIndexes.findIndex(el => el === this.state.index) + 1;
+    let myNewImgIndex = this.state.allImgIndexes.findIndex(el => el === this.state.indexImg) + 1;
+
+    console.log("MY NEW INDEX", myNewIndex)
+    console.log("this.state.allIndexes", this.state.allIndexes)
+    console.log("this.state.allIndexes[myNewIndex]", this.state.allIndexes[myNewIndex])
+    console.log("LAST POSSIBLE INDEX", this.state.allIndexes.length - 1)
+
+
+    if (this.state.allIndexes[myNewIndex]) {
+      let myNewIndexStory = this.state.allImgIndexes[myNewIndex];
+      let myNewImgIndexStory = this.state.allImgIndexes[myNewImgIndex];
+
+      this.setState({
+        index: myNewIndexStory,
+        indexImg: myNewImgIndexStory
+      }, () => {
+        document.querySelector(".bg").classList.add("animate")
+        document.querySelector(".blockquote").classList.add("animate")
+      })
+    } else {
+
+      this.handleClick()
+
+    }
+
+
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        {this.state.index &&
+          <div id="quote-box" className="blockquote-wrapper">
+
+            <div key={"img" + this.state.indexImg} className="bg"
+              style={{ backgroundImage: `linear-gradient(to bottom, rgba(255, 2, 252, 0), rgba(0, 0, 0, 0.73)), url(/bg/${images[this.state.indexImg]})` }}></div>
+
+            <div className="blockquote" key={this.state.index}>
+              <span className="quotes">‟</span>
+              <h1 id="text"> {quotes[this.state.index].quote} </h1>
+              <div className="arrows">
+              <h2 id="author">{quotes[this.state.index].author}</h2>
+              <div id="old-quote" onClick={() => this.handleClickPrev()} className="arrow left"></div>
+              <div id="new-quote" onClick={() => this.handleClickNext()} className="arrow right"></div>
+            </div>
+             
+            </div>
+
+            
+            <a href={`https://twitter.com/intent/tweet?text=${quotes[this.state.index].quote} ${quotes[this.state.index].author}.`} id="tweet-quote" target="_blank" rel="noreferrer">Tweet it</a>
+             
+          </div>
+
+        }
+
+      </div>
+    );
+
+  }
+
+
 }
 
 export default App;
---
+++ src/images.jsx
@@ -0,0 +1,19 @@
+
+const images= [
+  "pexels-frans-van-heerden-624015.jpg",
+  "pexels-eberhard-grossgasteiger-572897.jpg",
+  "pexels-tobias-bjørkli-2113566.jpg",
+  "pexels-ünsal-demirbaş-23547.jpg",
+  "pexels-jacub-gomez-1142950.jpg",
+  "pexels-walid-ahmad-1509582.jpg",
+  "pexels-frank-cone-2258536.jpg",
+  "pexels-tina-nord-804049.jpg",
+  "pexels-jeremy-bishop-2922672.jpg",
+  "pexels-asad-photo-maldives-3293148.jpg",
+  "pexels-paul-ijsendoorn-33041.jpg",
+  "pexels-andy-vu-3244513.jpg",
+  "pexels-ruvim-miksanskiy-1438761.jpg",
+  "pexels-luis-del-río-15286.jpg",
+  "pexels-stein-egil-liland-3408744.jpg",
+]
+export default images;
